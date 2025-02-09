8ae098f937ffcf408e0becf18a14c90c9af65dd4
Dhvanitmonpara <monparadhvanit@gmail.com>
2025-02-09 18:58:00 +0530

    added dialog box for confirmation to leave the interview

--- client/src/pages/InterviewPage.tsx
+++ client/src/pages/InterviewPage.tsx
@@ -1,5 +1,6 @@
 import Webcam from "@/components/interview/Webcam";
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -15,16 +16,32 @@ function InterviewPage() {
     }
   };
 
-  const leaveInterviewHandler = () => {
-    navigate("/dashboard")
-  }
-
   return (
     <div className="">
       <h3>Interview Analysis</h3>
       <div className="flex space-x-2">
         <Button>Skip time</Button>
-        <Button variant="destructive" onClick={leaveInterviewHandler}>Leave</Button>
+        <Dialog>
+          <DialogTrigger>
+            <span className="bg-red-500 text-zinc-100 rounded-md py-3 px-4">Leave</span>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you sure to leave?</DialogTitle>
+              <DialogDescription>
+                You are about to leave the interview. And your all data will be lost.
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter className="sm:justify-start">
+              <Button type="button" variant="destructive" onClick={() => navigate("/dashboard")}>
+                Leave
+              </Button>
+              <DialogClose className="h-full flex justify-center items-center bg-zinc-200 cursor-pointer px-3 rounded-md" asChild>
+                <span className="text-zinc-900 text-sm">Close</span>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
       <Webcam onEmotionalStateChange={handleEmotionalStateChange} />
       <div className="mt-4 px-4 text-xl">
