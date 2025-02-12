af95db790b1d131cad89f85faaff8ee955a4bdd7
Ujjwal <ujjwalsinghgaekwad@gmail.com>
2025-02-12 23:33:40 +0530

    feat: Add Speech Recognition

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,4 +1,5 @@
 import CodeEditor from "@/components/general/CodeEditor"
+import SpeechRecognition from "@/components/general/SpeechRecognition"
 import { Button } from "@/components/ui/button"
 import { useNavigate } from "react-router-dom"
 
@@ -14,9 +15,12 @@ function DashboardPage() {
     <div className="h-full w-full">
       <p className="text-white dark:text-black">dashboard</p>
       <Button onClick={sessionHandler}>Start session</Button>
-        <div className="p-4 rounded-md">
-          <CodeEditor />
-        </div>
+      {/* <div className="p-4 rounded-md">
+        <CodeEditor />
+      </div> */}
+      <div className="h-56 w-56 bg-gray-700">
+        <SpeechRecognition />
+      </div>
     </div>
   )
 }
