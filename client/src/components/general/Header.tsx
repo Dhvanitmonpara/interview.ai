a6d605b485d477ecdf2074484465a07f988fe1be
Henil <shahenil20@outlook.com>
2025-02-14 18:27:22 +0530

    Logo change

--- client/src/components/general/Header.tsx
+++ client/src/components/general/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
     <>
       <nav className="fixed h-16 border-b-2 border-gray-200 dark:border-gray-800 px-32 top-0 left-0 w-full flex justify-between items-center p-4">
         <Link to="/">
-          <img src="/logo.svg" alt="logo" />
+          <img className="h-14 w-14" src="/Logo.png" alt="logo" />
         </Link>
         <ul className="flex gap-4">
           <NavLink to="/" className={({ isActive }) => (`font-semibold ${isActive ? "text-zinc-950 dark:text-zinc-100" : "text-gray-500 dark:text-zinc-600"}`)}>Home</NavLink>
